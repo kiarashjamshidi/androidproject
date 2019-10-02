d5555ade43eceb0332ed20e3565e8464ace0a1a8
usi <usi@usis-MBP.mobile.usilu.net>
2019-10-02 13:26:36 +0200

    commitandroidproject

---
+++ Iraliani_H01_Sergio_Cucinotta/app/src/main/java/android/example/com/MainActivity.java
@@ -0,0 +1,13 @@
+package android.example.com;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
