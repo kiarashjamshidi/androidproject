d19f23eec73ff724d042034e58abbc055e25c43a
Kiarash <kiarash.jamshidi@usi.ch>
2019-10-17 18:22:16 +0200

    assign3 building

---
+++ app/src/androidTest/java/com/example/iraliani_h02_kiarash_jamshidi/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.iraliani_h02_kiarash_jamshidi;
+
+import android.content.Context;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.iraliani_h02_kiarash_jamshidi", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/example/iraliani_h02_kiarash_jamshidi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.iraliani_h02_kiarash_jamshidi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
