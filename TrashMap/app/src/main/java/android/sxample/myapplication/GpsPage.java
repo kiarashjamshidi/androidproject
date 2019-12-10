fcdbe2af39fef7a71b561eab2d5c9ddeb70c85ee
kiarashjamshidi <kiarash.jamshidi@usi.ch>
2019-12-10 15:57:55 +0100

    first coomit by kia

---
+++ TrashMap/app/src/main/java/android/sxample/myapplication/GpsPage.java
@@ -0,0 +1,14 @@
+package android.sxample.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GpsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_page);
+    }
+}
